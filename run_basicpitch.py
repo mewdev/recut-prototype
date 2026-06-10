84aa196d73c1f4119f8840d88b0db497883dd948
mewdev <michal.wierzgon@gmail.com>
2026-06-10 21:13:07 +0200

    chore: cleanup

--- run_basicpitch.py
+++
@@ -1,8 +0,0 @@
-import pathlib, sys
-audio = sys.argv[1]
-out = sys.argv[2]
-bp_model = pathlib.Path(__import__('basic_pitch').__file__).parent / 'saved_models/icassp_2022/nmp.onnx'
-from basic_pitch.inference import predict
-_, midi_data, _ = predict(audio, model_or_model_path=bp_model, minimum_note_length=180, onset_threshold=0.5, frame_threshold=0.3)
-midi_data.write(out)
-print(f'saved {out}')
--- testing/01/user-prompt.md
+++
@@ -1,1 +0,0 @@
-Ahoj, tak prosím nechej mi z té písničky jen část od 0:51 do 1:29, zbytek smazat. Moc díky.
