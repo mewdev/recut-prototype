c0a62434773967e1b04bbd6b9bd429c300da41b8
mewdev <michal.wierzgon@gmail.com>
2026-07-23 12:59:16 +0200

    feat: add audio class and its methods

---
+++ audio.py
@@ -0,0 +1,28 @@
+#TODO: make sure you understand this code more in depth (classes, self...)
+
+import numpy as np
+from librosa import load
+
+class Audio:
+    def __init__(self, samples: np.ndarray, sr:int) -> None:
+        self.samples = samples
+        self.sr = sr
+
+    @property
+    def is_mono(self) -> bool:
+        return self.samples.ndim == 1
+
+    @property
+    def num_samples(self) -> int:
+        return self.samples.shape[-1]
+
+    def apply_to_channels(self, fn) -> "Audio":
+        if self.is_mono:
+            return Audio(fn(self.samples), self.sr)
+
+        return Audio(np.stack([fn(self.samples[0]), fn(self.samples[1])]), self.sr)
+
+    @classmethod
+    def load(cls, path:str) -> "Audio":
+        samples, sr = load(path, sr=None)
+        return cls(samples, int(sr))
