74420aa18ef8247db7659f0478a3202240608bc8
mewdev <michal.wierzgon@gmail.com>
2026-08-05 19:20:31 +0200

    feat: add chain primitive

---
+++ primitives/chain.py
@@ -0,0 +1,8 @@
+from audio import Audio
+
+def chain(audio: Audio, *transforms) -> Audio:
+    for transform in transforms:
+        audio = transform(audio)
+        if not isinstance(audio, Audio):
+            raise TypeError(f"transform {transform} returned {type(audio)}, expected Audio")
+    return audio
